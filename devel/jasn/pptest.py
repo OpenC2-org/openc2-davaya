f07b671aee1a8356bcf2757e6579496fbed16aab
Dave Kemp <dpkemp@missi.ncsc.mil>
2016-09-23 16:56:12 -0400

    pasn parser test

--- devel/jasn/pptest.py
+++ devel/jasn/pptest.py
@@ -1,23 +1,29 @@
 import pyparsing as pp
 
 instring = """
--- This is (a) #%^& comment
 
 /*
 name1: val
 name2: val2 with $*&#@) junk
+       line2: content
 name3: val3: with @)(*% multi-
-       line: content
+       line3: content
 */
 """
 
-comment1 = pp.Literal("--") + pp.originalTextFor(pp.SkipTo(pp.LineEnd())).setDebug()
-identifier = pp.Word(pp.alphanums + "_").setDebug()
-meta1 = pp.LineStart() + identifier + pp.Literal(":") + pp.SkipTo(pp.LineEnd())
-meta2 = pp.LineStart() + pp.White() + pp.SkipTo(pp.LineEnd())
-metaval = meta1 + pp.ZeroOrMore(meta2)
-metalist = pp.ZeroOrMore(comment1) + pp.Literal("/*") + pp.OneOrMore(metaval) + pp.Literal("*/")
+metakey = pp.Word(pp.alphanums + "_")
+metakey.addCondition(lambda s,loc,toks: pp.col(loc,s) == 1)
+c_open = pp.Literal("/*").suppress()
+c_close = pp.Literal("*/").suppress()
+meta1 = metakey + pp.Literal(":").suppress() + pp.SkipTo(pp.LineEnd())
+meta2 = pp.White().suppress() + pp.SkipTo(pp.LineEnd())
+meta_end = metakey | c_close
+# meta_end = pp.Literal("nam") | c_close
+metaval = pp.Group(meta1 + pp.ZeroOrMore(meta2, stopOn=meta_end)).setDebug()
+metalist = c_open + pp.OneOrMore(metaval) + c_close
 
 if __name__ == "__main__":
     p = metalist.parseString(instring)
-    print(p)
+    print("Result:")
+    for pv in p:
+        print(" " + str(pv))
