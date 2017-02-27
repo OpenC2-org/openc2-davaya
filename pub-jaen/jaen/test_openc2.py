fe254bf1aeba4f531d444c4f23a2ee84ffdb8431
Dave Kemp <dpkemp@missi.ncsc.mil>
2017-02-27 17:02:38 -0500

    flatten api

--- pub-jaen/jaen/test_openc2.py
+++ pub-jaen/jaen/test_openc2.py
@@ -3,6 +3,7 @@ import unittest
 
 from jaen.codec.codec import Codec
 from jaen.codec.jaen import jaen_load
+from jaen.codec.codec_utils import flatten, fluff
 
 
 class OpenC2(unittest.TestCase):
@@ -12,23 +13,26 @@ class OpenC2(unittest.TestCase):
         self.tc = Codec(jaen)
 
     def test_query1(self):
-        cmd1_api = {"action": "query", "target": {"commands":""}}
-        cmd1_flat = {"action": "query", "target.commands":""}
+        cmd1_min = [3,{"2":""}]
         cmd1_concise = ["query", {"commands":""}]
         cmd1_noname = {"1":3, "2":{"2":""}}
-        cmd1_min = [3,{"2":""}]
+        cmd1_api = {"action": "query", "target": {"commands":""}}
+        cmd1_flat = {"action": "query", "target.commands":""}
+
                                         # Minified (list/tag)
         self.assertEqual(self.tc.decode("OpenC2Command", cmd1_min), cmd1_api)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_min)
-        self.tc.set_mode(True, False)   # not used, but check dict/tag mode anyway
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_noname), cmd1_api)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_noname)
         self.tc.set_mode(False, True)   # Concise (list/name)
         self.assertEqual(self.tc.decode("OpenC2Command", cmd1_concise), cmd1_api)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_concise)
-        self.tc.set_mode(True, True)    # Verbose (dict/name)
+        self.tc.set_mode(True, False)   # not used, but check dict/tag mode anyway
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_noname), cmd1_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_noname)
+        self.tc.set_mode(True, True)    # API / Verbose (dict/name)
         self.assertEqual(self.tc.decode("OpenC2Command", cmd1_api), cmd1_api)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_api)
+        self.assertEqual(flatten(cmd1_api), cmd1_flat)
+        self.assertEqual(fluff(cmd1_flat), cmd1_api)
 
     def test_query2(self):
         '''
@@ -70,16 +74,9 @@ class OpenC2(unittest.TestCase):
         }
         '''
 
-        cmd2_api = {           # Type as property name
-            "action": "scan",
-            "target": {
-                "domain-name": {
-                    "value": "www.example.com",
-                    "resolves_to": [
-                        {"v4": {"value": "198.51.100.2"}},
-                        {"name": {"value": "ms34.example.com"}}]}}}
+        cmd2_min = [1,{"7":["www.example.com",[{"1":["198.51.100.2"]},{"3":["ms34.example.com"]}]]}]
 
-        cmd2_concise = [
+        cmd2_concise = [        # Concise (list/name)
             "scan",
             {
                 "domain-name": [
@@ -88,7 +85,7 @@ class OpenC2(unittest.TestCase):
                         {"v4": ["198.51.100.2"]},
                         {"name": ["ms34.example.com"]}]]}]
 
-        cmd2_noname = {
+        cmd2_noname = {         # unused (dict/tag)
             "1": 1,
             "2": {
                 "7": {
@@ -97,7 +94,21 @@ class OpenC2(unittest.TestCase):
                         {"1":{"1": "198.51.100.2"}},
                         {"3":{"1": "ms34.example.com"}}]}}}
 
-        cmd2_min = [1,{"7":["www.example.com",[{"1":["198.51.100.2"]},{"3":["ms34.example.com"]}]]}]
+        cmd2_api = {           # API / Verbose (dict/name)
+            "action": "scan",
+            "target": {
+                "domain-name": {
+                    "value": "www.example.com",
+                    "resolves_to": [
+                        {"v4": {"value": "198.51.100.2"}},
+                        {"name": {"value": "ms34.example.com"}}]}}}
+
+        cmd2_flat = {           # Flattened
+            "action": "scan",
+            "target.domain-name.value": "www.example.com",
+            "target.domain-name.resolves_to": [
+                {"v4.value": "198.51.100.2"},
+                {"name.value": "ms34.example.com"}]}
 
                                             # Minified (list/tag)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_min)
@@ -108,9 +119,12 @@ class OpenC2(unittest.TestCase):
         self.tc.set_mode(True, False)       # unused (dict/tag)
         self.assertEqual(self.tc.decode("OpenC2Command", cmd2_noname), cmd2_api)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_noname)
-        self.tc.set_mode(True, True)        # Verbose (dict/name)
+        self.tc.set_mode(True, True)        # API / Verbose (dict/name)
         self.assertEqual(self.tc.decode("OpenC2Command", cmd2_api), cmd2_api)
         self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_api)
+        cf = flatten(cmd2_api)
+        self.assertEqual(flatten(cmd2_api), cmd2_flat)
+        self.assertEqual(fluff(cmd2_flat), cmd2_api)
 
 if __name__ == "__main__":
     unittest.main()
