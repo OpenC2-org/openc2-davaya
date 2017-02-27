7bb3dfced4ddd629dce6fc7b99b824a8f7433044
Dave Kemp <dpkemp@missi.ncsc.mil>
2017-02-27 15:05:39 -0500

    Working codec and openc2 unit tests

--- pub-jaen/jaen/test_codec.py
+++ pub-jaen/jaen/test_codec.py
@@ -147,16 +147,17 @@ class BasicTypes(unittest.TestCase):
     C2a = {"type2": False}
     C3a = {"type3": 42}
     C4a = {"type4": {"a": 1, "b": "c"}}
-    C1m = {1: "foo"}
-    C2m = {4: False}
-    C3m = {7: 42}
-    C4m = {9:[1,"c"]}
+    C1m = {"1": "foo"}
+    C2m = {"4": False}
+    C3m = {"7": 42}
+    C4m = {"9":[1,"c"]}
     C1_bad1a = {"type1": 15}
     C1_bad2a = {"type5": "foo"}
     C1_bad3a = {"type1": "foo", "type2": False}
-    C1_bad1m = {1: 15}
-    C1_bad2m = {3: "foo"}
-    C1_bad3m = {1: "foo", 4: False}
+    C1_bad1m = {"1": 15}
+    C1_bad2m = {"3": "foo"}
+    C1_bad3m = {"1": "foo", "4": False}
+    C1_bad4m = {1: "foo"}
 
     def test_choice_min(self):
         self.assertEqual(self.tc.decode("t_choice", self.C1m), self.C1a)
@@ -173,6 +174,8 @@ class BasicTypes(unittest.TestCase):
             self.tc.decode("t_choice", self.C1_bad2m)
         with self.assertRaises(ValueError):
             self.tc.decode("t_choice", self.C1_bad3m)
+        with self.assertRaises(ValueError):
+            self.tc.decode("t_choice", self.C1_bad4m)
         with self.assertRaises(TypeError):
             self.tc.encode("t_choice", self.C1_bad1a)
         with self.assertRaises(ValueError):
@@ -249,13 +252,14 @@ class BasicTypes(unittest.TestCase):
     RGB_bad6a = {"red": 24, "green": 120, "bleu": 240}
     RGB_bad7a = {2: 24, "green": 120, "blue": 240}
 
-    Map1m = {2: 24, 4: 120, 6: 240}                  # Encoded values Map (minimized and dict/tag mode)
-    Map2m = {2: 50, 6: 100}
-    Map3m = {2: 9, 4: 80, 6: 96, 9: 128}
-    Map_bad1m = {2: 24, 4: 120}
-    Map_bad2m = {2: 9, 4: 80, 6: 96, 9: 128, 12: 42}
-    Map_bad3m = {2: "four", 4: 120, 6: 240}
-    Map_bad4m = {"2": 24, 4: 120, 6: 240}
+    Map1m = {"2": 24, "4": 120, "6": 240}                  # Encoded values Map (minimized and dict/tag mode)
+    Map2m = {"2": 50, "6": 100}
+    Map3m = {"2": 9, "4": 80, "6": 96, "9": 128}
+    Map_bad1m = {"2": 24, "4": 120}
+    Map_bad2m = {"2": 9, "4": 80, "6": 96, "9": 128, "12": 42}
+    Map_bad3m = {"2": "four", "4": 120, "6": 240}
+    Map_bad4m = {2: 24, 4: 120, 6: 240}
+    Map_bad5m = [24, 120, 240]
 
     Rec1m = [24, 120, 240]                          # Encoded values Record (minimized)
     Rec2m = [50, None, 100]
@@ -264,13 +268,13 @@ class BasicTypes(unittest.TestCase):
     Rec_bad2m = [9, 80, 96, 128, 42]
     Rec_bad3m = ["four", 120, 240]
 
-    Rec1n = {1: 24, 2: 120, 3: 240}                  # Encoded values Record (unused dict/tag mode)
-    Rec2n = {1: 50, 3: 100}
-    Rec3n = {1: 9, 2: 80, 3: 96, 4: 128}
-    Rec_bad1n = {1: 24, 2: 120}
-    Rec_bad2n = {1: 9, 2: 80, 3: 96, 4: 128, 5: 42}
-    Rec_bad3n = {1: "four", 2: 120, 3: 240}
-    Rec_bad4n = {"1": 24, 2: 120, 3: 240}
+    Rec1n = {"1": 24, "2": 120, "3": 240}                  # Encoded values Record (unused dict/tag mode)
+    Rec2n = {"1": 50, "3": 100}
+    Rec3n = {"1": 9, "2": 80, "3": 96, "4": 128}
+    Rec_bad1n = {"1": 24, "2": 120}
+    Rec_bad2n = {"1": 9, "2": 80, "3": 96, "4": 128, "5": 42}
+    Rec_bad3n = {"1": "four", "2": 120, "3": 240}
+    Rec_bad4n = {1: 24, 2: 120, 3: 240}
 
     RGB1c = [24, 120, 240]                           # Encoded values Record (concise)
     RGB2c = [50, None, 100]
@@ -294,6 +298,8 @@ class BasicTypes(unittest.TestCase):
             self.tc.decode("t_map", self.Map_bad3m)
         with self.assertRaises(ValueError):
             self.tc.decode("t_map", self.Map_bad4m)
+        with self.assertRaises(TypeError):
+            self.tc.decode("t_map", self.Map_bad5m)
         with self.assertRaises(ValueError):
             self.tc.encode("t_map", self.RGB_bad1a)
         with self.assertRaises(ValueError):
--- pub-jaen/jaen/test_openc2.py
+++ pub-jaen/jaen/test_openc2.py
@@ -11,8 +11,29 @@ class OpenC2(unittest.TestCase):
         jaen = jaen_load(os.path.join("data", "openc2.jaen"))
         self.tc = Codec(jaen)
 
-    def test_query(self):
-        cmd2_api1 = {           # Literal STIX Object
+    def test_query1(self):
+        cmd1_api = {"action": "query", "target": {"commands":""}}
+        cmd1_flat = {"action": "query", "target.commands":""}
+        cmd1_concise = ["query", {"commands":""}]
+        cmd1_noname = {"1":3, "2":{"2":""}}
+        cmd1_min = [3,{"2":""}]
+                                        # Minified (list/tag)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_min), cmd1_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_min)
+        self.tc.set_mode(True, False)   # not used, but check dict/tag mode anyway
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_noname), cmd1_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_noname)
+        self.tc.set_mode(False, True)   # Concise (list/name)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_concise), cmd1_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_concise)
+        self.tc.set_mode(True, True)    # Verbose (dict/name)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_api), cmd1_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_api)
+
+    def test_query2(self):
+        '''
+        # Alternate Target Data Model Schemas
+        cmd2_api2 = {           # Literal STIX Object
             "action": "scan",
             "target": {
                 "0": {
@@ -31,7 +52,7 @@ class OpenC2(unittest.TestCase):
             }
         }
 
-        cmd2_api2 = {           # Type-Specifiers
+        cmd2_api3 = {           # Type-Specifiers
             "action": "scan",
             "target": {
                 "type": "domain-name",
@@ -47,54 +68,49 @@ class OpenC2(unittest.TestCase):
                 }
             }
         }
+        '''
 
-        cmd2_api3 = {           # Type as property name
+        cmd2_api = {           # Type as property name
             "action": "scan",
             "target": {
                 "domain-name": {
                     "value": "www.example.com",
                     "resolves_to": [
                         {"v4": {"value": "198.51.100.2"}},
-                        {"name": {"value": "ms34.example.com"}}
-                    ]
-                }
-            }
-        }
+                        {"name": {"value": "ms34.example.com"}}]}}}
 
-        cmd2_min3 = [1,{"7":["www.example.com",[{"1":["198.51.100.2"]},{"3":["ms34.example.com"]}]]}]
-        cmd2_min3 = [1,{7:["www.example.com",[{1:["198.51.100.2"]},{3:["ms34.example.com"]}]]}]
+        cmd2_concise = [
+            "scan",
+            {
+                "domain-name": [
+                    "www.example.com",
+                    [
+                        {"v4": ["198.51.100.2"]},
+                        {"name": ["ms34.example.com"]}]]}]
 
-        t_api3m = self.tc.encode("OpenC2Command", cmd2_api3)
+        cmd2_noname = {
+            "1": 1,
+            "2": {
+                "7": {
+                    "1": "www.example.com",
+                    "2": [
+                        {"1":{"1": "198.51.100.2"}},
+                        {"3":{"1": "ms34.example.com"}}]}}}
+
+        cmd2_min = [1,{"7":["www.example.com",[{"1":["198.51.100.2"]},{"3":["ms34.example.com"]}]]}]
+
+                                            # Minified (list/tag)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_min)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_min), cmd2_api)
+        self.tc.set_mode(False, True)       # Concise (list/name)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_concise), cmd2_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_concise)
+        self.tc.set_mode(True, False)       # unused (dict/tag)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_noname), cmd2_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_noname)
         self.tc.set_mode(True, True)        # Verbose (dict/name)
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_api3), cmd2_api3)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api3), cmd2_api3)
-#        self.tc.set_mode(False, True)        # Concise (list/name)
-#        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_min3), cmd2_api3)
-#        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api3), cmd2_min3)
-#        self.tc.set_mode(True, False)        # unused (dict/tag)
-#        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_min3), cmd2_api3)
-#        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api3), cmd2_min3)
-        self.tc.set_mode(False, False)        # Minified (list/tag)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api3), cmd2_min3)
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_min3), cmd2_api3)
-
-        cmd1_api = {"action": "query", "target": {"commands":{}}}
-        cmd1_flat = {"action": "query", "target.commands":{}}
-        cmd1_concise = ["query", ["commands"]]
-        cmd1_noname = {"1":3, "2":{"1":2}}
-        cmd1_min = [3,{"2":""}]
-                                        # Minified (list/tag)
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_min), cmd1_api)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_min)
-        self.tc.set_mode(True, False)   # not named, but check dict/tag mode anyway
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_noname), cmd1_api)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_noname)
-        self.tc.set_mode(False, True)   # Concise (list/name)
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_concise), cmd1_api)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_concise)
-        self.tc.set_mode(True, True)    # Verbose (dict/name)
-        self.assertEqual(self.tc.decode("OpenC2Command", cmd1_api), cmd1_api)
-        self.assertEqual(self.tc.encode("OpenC2Command", cmd1_api), cmd1_api)
+        self.assertEqual(self.tc.decode("OpenC2Command", cmd2_api), cmd2_api)
+        self.assertEqual(self.tc.encode("OpenC2Command", cmd2_api), cmd2_api)
 
 if __name__ == "__main__":
     unittest.main()
